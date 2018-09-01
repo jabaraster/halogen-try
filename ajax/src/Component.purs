913c1dc6c6e7776b1e486dcfd2376c4a20133b00
jabaraster <ah@jabara.info>
2018-09-02 08:33:14 +0900

    checkpoint.

--- ajax/src/Component.purs
+++ ajax/src/Component.purs
@@ -1,14 +1,19 @@
 module Component where
 
+import           Prelude
+
+import           Affjax                       as AX
+import           Affjax.ResponseFormat        as RF
+
+import           Data.Either                  (Either(..))
 import           Data.Maybe                   (Maybe (..))
+
 import           Effect.Aff                   (Aff)
+
 import           Halogen                      as H
 import           Halogen.HTML                 as HH
 import           Halogen.HTML.Events          as HE
 import           Halogen.HTML.Properties      as HP
-import           Affjax                       as AX
-import           Affjax.ResponseFormat
-import           Prelude
 
 type State = { loading :: Boolean
              , username :: String
@@ -27,27 +32,51 @@ ui =
               }
 
 initialState :: State
-initialState = { loading: true
+initialState = { loading: false
                , username: ""
                , result: Nothing
                }
 
 render :: State -> H.ComponentHTML Query
 render st =
-  HH.form_ $
-    [ HH.h1_ [ HH.text "Lookup Github user" ]
-    ]
+    HH.form_ $
+      [ HH.h1_ [ HH.text "Lookup GitHub user" ]
+      , HH.label_
+          [ HH.div_ [ HH.text "Enter username:" ]
+          , HH.input
+              [ HP.value st.username
+              , HE.onValueInput (HE.input SetUsername)
+              ]
+          ]
+      , HH.button
+          [ HP.disabled st.loading
+          , HP.type_ HP.ButtonButton
+          , HE.onClick (HE.input_ MakeRequest)
+          ]
+          [ HH.text "Fetch info" ]
+      , HH.p_
+          [ HH.text (if st.loading then "Working..." else "") ]
+      , HH.div_
+          case st.result of
+            Nothing -> []
+            Just res ->
+              [ HH.h2_
+                  [ HH.text "Response:" ]
+              , HH.pre_
+                  [ HH.code_ [ HH.text res ] ]
+              ]
+      ]
 
 eval ::  Query ~> H.ComponentDSL State Query Void Aff
 eval (SetUsername username next) = do
     H.modify_ (_ { username = username, result = Nothing })
     pure next
 eval (MakeRequest next) = do
-    H.modify_ (_ { username = "request", result = Nothing })
-    pure next
-eval (MakeRequest next) = do
     username <- H.gets _.username
     H.modify_ (_ { loading = true })
-    response <- H.liftAff $ AX.get json ("https://api.github.com/users/" <> username)
-    H.modify_ (_ { loading = false, result = Just response.response })
-    pure next
+    res <- H.liftAff $ AX.get RF.string ("https://api.github.com/users/" <> username)
+    case res.body of
+        Left _     -> pure next
+        Right json -> do
+            H.modify_ (_ { loading = false, result = Just json })
+            pure next
